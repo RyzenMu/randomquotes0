cf2749f84acb329e313bcf4c9af09edbde564cdb
RyzenMu <muruga13in2022@gmail.com>
2023-01-12 21:20:57 +0530

    rondom quotes

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ import data from './quotes.json' assert {type : 'json'};
 console.log(data);
 
 console.log(data.quotes[12].quote+'--'+data.quotes[12].author);
+
+
+// import data from MOCK_DATA
+
+import data1 from './MOCK_DATA.json' assert { type : 'json'};
+
+
+
+let random = Math.floor(Math.random()*1000);
+
+console.log(data1.user[random].first_name);
+
+console.log(random);
