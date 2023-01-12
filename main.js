1a744c01bd46715796ec73c91efa6f8c203407f2
RyzenMu <muruga13in2022@gmail.com>
2023-01-12 21:01:55 +0530

    json

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 
 
-// import data from json file
+// import quotes data from json file
 
 import data from './quotes.json' assert {type : 'json'};
 
 console.log(data);
 
-console.log(data.quotes[100].quote+'--'+data.quotes[100].author);
+console.log(data.quotes[12].quote+'--'+data.quotes[12].author);
